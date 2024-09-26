2e45d82b492b3c37ee9e4a30a71316e1e6a56acc
lexa-illi <Alexander.Williams@my.mrc.tas.edu.au>
2024-09-27 09:34:11 +1000

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://lexa-illi.github.io/habitat-runner/](https://lexa-illi.github.io/habitat-runner/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/lexa-illi/habitat-runner** and import
+
+## Edit this project ![Build status badge](https://github.com/lexa-illi/habitat-runner/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/lexa-illi/habitat-runner** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/lexa-illi/habitat-runner/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,119 @@
+namespace SpriteKind {
+    export const info = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    controller.moveSprite(mySprite, 0, 0)
+    pause(2000)
+    mySprite.setPosition(43, 20)
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy == 0) {
+        mySprite.vy = -150
+    }
+    if (Habitat == 0) {
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`penguin jumping`,
+        100,
+        false
+        )
+    }
+    if (Habitat == 1) {
+    	
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`egg 3`, function (sprite, location) {
+    scene.setBackgroundImage(assets.image`Snowy Background`)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    mySprite.setPosition(43, 20)
+    level += 1
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Habitat == 0) {
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`penguin sliding left`,
+        100,
+        false
+        )
+    }
+    if (Habitat == 1) {
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`penguin sliding left`,
+        100,
+        false
+        )
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    if (Habitat == 0) {
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`penguin standing right`,
+        75,
+        false
+        )
+    }
+    if (Habitat == 1) {
+    	
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    if (Habitat == 0) {
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`penguin standing left`,
+        75,
+        false
+        )
+    }
+    if (Habitat == 1) {
+    	
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Habitat == 0) {
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`penguin sliding right`,
+        100,
+        false
+        )
+    }
+    if (level == 1) {
+    	
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`egg two`, function (sprite, location) {
+    scene.setBackgroundImage(assets.image`Snowy Background`)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    mySprite.setPosition(43, 20)
+    level += 1
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Egg one`, function (sprite, location) {
+    scene.setBackgroundImage(assets.image`Snowy Background`)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    mySprite.setPosition(116, 20)
+    level += 1
+})
+let level = 0
+let Habitat = 0
+let mySprite: Sprite = null
+game.splash("Habitat Runner")
+game.splash("Use the left and right arrow keys to move")
+game.splash("Coins can be used to buy items")
+game.splash("When in the store, use the A button to choose which item to buy and the B button to buy that item.")
+game.splash("Use the up arrow key to jump and the A button to use your item")
+scene.setBackgroundImage(assets.image`Snowy Background`)
+tiles.setCurrentTilemap(tilemap`level1`)
+mySprite = sprites.create(assets.image`Penguin still`, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 0)
+scene.cameraFollowSprite(mySprite)
+mySprite.setPosition(43, 20)
+let stats = sprites.create(assets.image`stats`, SpriteKind.info)
+stats.setPosition(80, 10)
+mySprite.ay = 300
+forever(function () {
+	
+})
